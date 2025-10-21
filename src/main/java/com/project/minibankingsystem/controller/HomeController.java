27d447e90292e6084fef8f435172343fa93e8767
Luthfi Saputra <luthfsptr@gmail.com>
2025-10-21 22:17:02 +0700

    new

---
+++ src/main/java/com/project/minibankingsystem/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.project.minibankingsystem.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "Walcome to Mini Banking System API";
+    }
+}
